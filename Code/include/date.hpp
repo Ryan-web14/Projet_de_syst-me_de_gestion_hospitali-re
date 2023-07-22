4545bc989a0f715962c61678c36f948729228ed6
Ryan <bikindou@gmail.com>
2023-07-22 07:28:37 +0100

    Continuation de l'implémentation de Date

--- Code/include/date.hpp
+++ Code/include/date.hpp
@@ -6,6 +6,18 @@
 class Date
 {
 
+public:
+//constructor
+//set month to one and year to 2023
+Date();
+
+//getter and setter 
+//getter
+int getYear() const;
+int getMonth() const;
+int getDay() const;
+
+
 private:
 int _year;
 int _month;
